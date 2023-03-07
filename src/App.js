716e43983edf7ae040a0a6e5faecbb980c664863
HKest <117974299+HKest@users.noreply.github.com>
2023-03-08 00:32:44 +0200

    nimi ei ole kodutoo1
    Sellepärast, et tegin ennem juba, kui meili sain kodutoo uue kataloogi

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <button className='nupp'>Nupp</button>
+ <button className='nupp'>Nupp</button>
 
-    <p className='sinine-tekst'>Tee sinine tekst, millele hiirega peale minnes muuda selle teksti suurus suuremaks </p>
-    <p className='roheline-tekst'>Tee roheline tekst</p>
-    <img src='https://cdn.cloudflare.steamstatic.com/steam/apps/1060870/header.jpg?t=1615951640' className='pilt' alt='Hallo World'/>
+ <p className='sinine-tekst'>Tee sinine tekst, millele hiirega peale minnes muuda selle teksti suurus suuremaks </p>
+ <p className='roheline-tekst'>Tee roheline tekst</p>
+ <img src='https://cdn.cloudflare.steamstatic.com/steam/apps/1060870/header.jpg?t=1615951640' className='pilt' alt='Hallo World'/>
     </div>
   );
 }
