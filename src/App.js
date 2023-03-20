c98a078c81e16b7ee5937146e0d6b8dd7e2aa884
HKest <117974299+HKest@users.noreply.github.com>
2023-03-20 20:30:43 +0200

    localStorage, Firebase, font/favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useRef } from 'react';
+
 
 function App() {
+
+const [sisseLogitud, muudaSisselogitud] = useState("ei");
+const [sonum, muudaSonum] = useState("");
+const kasutajaNimiRef = useRef();
+const paroolRef = useRef();
+
+const logiSisse = () => {
+  if (paroolRef.current.value === "123") {
+  muudaSisselogitud("jah");
+  muudaSonum("Olete sisse logitud");
+  muudaSonum(kasutajaNimiRef.current.value + " oled sisse logitud");
+
+} else { 
+   muudaSonum("Vale parool");
+}
+}
+
+const logiValja = () => {
+  muudaSisselogitud("ei");
+  muudaSonum("Olete välja logitud")
+}
+
+
+
+
   return (
     <div className="App">
+
+      <div>{sonum}</div>
+
+{sisseLogitud === "ei" &&<div>
+      <label>Kasutajanimi</label><br />
+      <input ref={kasutajaNimiRef} type="text" /><br />
+      <label>Parool</label><br />
+      <input ref={paroolRef} type="password" /><br />
+      </div>}
+
+{sisseLogitud === "ei" &&<button onClick={logiSisse}>Logi sisse</button>}
+{sisseLogitud === "jah" &&<button onClick={ logiValja }>Logi välja</button>}
+
+
+<br />
+<br />
+{/* Siin algab esimine kodutöö */}
+
  <button className='nupp'>Nupp</button>
+
+
 
  <p className='sinine-tekst'>Tee sinine tekst, millele hiirega peale minnes muuda selle teksti suurus suuremaks </p>
  <p className='roheline-tekst'>Tee roheline tekst</p>
@@ -12,5 +59,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
